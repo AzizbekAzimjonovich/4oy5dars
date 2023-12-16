6091bb8529889ffdffdc868da0be378282d3c218
AzizbekAzimjonovich <azizbekazimjonovich@gmail.com>
2023-12-16 21:18:41 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,31 @@
+//1-misol
+/**let a=+prompt("son");
+let b=+prompt("son");
+let i=a;
+let hisob=1;
+while (i<=b) {
+    hisob*=i;
+    i++
+}
+console.log(hisob); */
+
+//2-misol
+/**let n=+prompt("son");
+let q;
+let hisob=0;
+while (1<=n) {
+    q=n%10
+    n=Math.trunc(n/10)
+    hisob++
+}
+console.log(hisob); */
+
+//3-misol
+let n=+prompt("son");
+let hisob=1;
+let i=1;
+while (i<=n) {
+    hisob*=i
+    i++
+}
+console.log(hisob);
